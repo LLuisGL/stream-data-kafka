bfadcdf4ba26848d307a6f1b70728f82f41a3e5f
Carlos <carlosalejandroauquimolina@gmail.com>
2025-12-14 16:07:54 -0500

    SEGUNDO COMMIT

--- data-faker/run.py
+++ data-faker/run.py
@@ -1,15 +1,15 @@
 import json
 import random
 import time
-from kafka import KafkaProducer
-from faker import Faker
+from kafka import KafkaProducer # type: ignore
+from faker import Faker # type: ignore
 from datetime import datetime
 
 fake = Faker()
 
 producer = KafkaProducer(
-    bootstrap_servers="localhost:9092",
-    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')  
 )
 
 CATEGORIES = ["Tech", "Home", "Toys", "Clothes", "Books"]
@@ -23,19 +23,19 @@ def generate_random_event():
     province = PROVINCES[CITIES.index(city)] if city in CITIES[:3] else random.choice(PROVINCES)
 
     return {
-        "user_id": random.randint(1, 1000),
-        "product_id": random.randint(1, 1000),
-        "category": random.choice(CATEGORIES),
-        "event_type": random.choice(EVENT_TYPES),
-        "price": round(random.uniform(5.00, 1500.00), 2),
-        "timestamp": datetime.now().isoformat() + "Z",
-        "store_id": f"ST{str(random.randint(1, 99)).zfill(3)}",
-        "store_type": random.choice(STORE_TYPES),
-        "city": city,
-        "province": province,
-        "location": {
-            "lat": float(fake.latitude()),
-            "lon": float(fake.longitude())
+        'user_id': random.randint(1, 1000),
+        'product_id': random.randint(1, 1000),
+        'category': random.choice(CATEGORIES),
+        'event_type': random.choice(EVENT_TYPES),
+        'price': round(random.uniform(5.00, 1500.00), 2),
+        'timestamp': datetime.now().isoformat() + "Z",
+        'store_id': f"ST{str(random.randint(1, 99)).zfill(3)}",
+        'store_type': random.choice(STORE_TYPES),
+        'city': city,
+        'province': province,
+        'location': {
+            'lat': float(fake.latitude()),
+            'lon': float(fake.longitude())
         }
     }
 
@@ -44,7 +44,7 @@ topic_name = "stream_in"
 try:
     while True:
         event = generate_random_event()
-        producer.send(topic_name, event)
+        producer.send(topic_name, value=event)
         print("Enviado:", event)
         time.sleep(1)
 except KeyboardInterrupt:
